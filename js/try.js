f7cde411d714f2651d9daed6a8f2d8e4b177d28f
wisdomfree <739811128@qq.com>
2016-03-09 23:08:53 +0800

    Update try.js
    设置section1的初始渐变效果，使刚加载好的section1能平滑过渡到section2

--- js/try.js
+++ js/try.js
@@ -9,6 +9,14 @@ $(function(){
 		// Design
 		sectionsColor: ['#37df5f', '#58a8ec', '#ff5ca0'],
 		verticalCentered: false,
+		afterLoad: function (anchorLink,index){
+			// 加载不同屏时 调用并执行相应的动画函数
+			switch(index){
+				case 1:$(".section").css("background-color","#37df5f");break;
+				case 2:break;
+				case 3:break;
+			}
+		},
 		// Onleave
 		onLeave: function (index,nextIndex,direction){
 			// index背景颜色渐变成nextIndex的背景颜色 获得融合效果
