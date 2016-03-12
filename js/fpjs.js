4b63b39c646100625dd5d6bf155be0258ff407f0
wisdomfree <739811128@qq.com>
2016-03-12 14:24:18 +0800

    page2新版本提交
    更改动画和部分样式

--- js/fpjs.js
+++ js/fpjs.js
@@ -24,7 +24,11 @@ $(function(){
 						slider();
 						},10);
 						break;
-				case 2: 
+				case 2: b = 0;
+						c=1;
+						setTimeout(function(){
+							ray();
+						},1000);
 						break;
 				case 3: /*在第三屏时，设置鼠标hover效果*/
 				 $(function(){
@@ -33,11 +37,11 @@ $(function(){
 							});*/		
 							$(".team>h2").mouseover(function() {
 							$(this).next("ul").show();
-							$(".work>h2").next("ul").hide(0);
+							$(".work>h2").next("ul").hide(500);
 							});	
 							$(".work>h2").mouseover(function() {
 							$(this).next("ul").show();
-							$(".team>h2").next("ul").hide(0);
+							$(".team>h2").next("ul").hide(500);
 							});						
 						});
 						break;
@@ -58,7 +62,11 @@ $(function(){
 						slider();
 						},100);
 						break;
-				case 2: 
+				case 2:b = 1;
+						c=0;
+						setTimeout(function(){
+							ray1();
+						},0);
 						break;
 				case 3: /*离开第三屏，隐藏ul*/
 				$(".team>h2,.work>h2").next("ul").hide();
@@ -86,4 +94,30 @@ function slider(){
 		sp.className=sp.className.replace(/ s1-p-active/,"");
 	}
 
+}
+var b;
+var c;
+var tid;
+function ray(){
+	if(c) {
+		b += 0.1;
+		if (b == 1.00) {
+			clearTimeout(tid);
+		}
+	}
+	document.getElementById('team').style.opacity = b;
+	document.getElementById('intro').style.opacity = b;
+	tid=setTimeout(function(){
+		ray();
+	},100);
+}
+function ray1(){
+	if(!c) {
+		b -= 1;
+		if (b == 0.00) {
+			clearTimeout(tid);
+		}
+	}
+	document.getElementById('team').style.opacity = b;
+	document.getElementById('intro').style.opacity = b;
 }
